83f33a39a07c0585f605a41fd64b68fe8f144ed7
jtesti <jtestill@gmail.com>
2022-02-28 18:03:59 +0100

    ejseis

---
+++ ejercicioseis.js
@@ -0,0 +1,16 @@
+for (var i = 0; i <= 9; i++) {
+    console.log(i);
+  }
+
+  for (var i = 0; i <= 9; i++) {
+    if(i / 2 === 0) {
+        console.log(i);
+    }
+  }
+
+  for(let i=0; i<=10; i++){
+    console.log('Intentando dormir');
+    if(i === 10) {
+      console.log('Dormido!')
+    }
+}
