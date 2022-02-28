06c5834a04e1365a1e69b29fadbbdc80fdd538f7
jtesti <jtestill@gmail.com>
2022-02-28 16:02:06 +0100

    ejerciciotres

---
+++ ejerciciotres.js
@@ -0,0 +1,21 @@
+let numA = 10;
+let numB = 5;
+alert(numA * numB);
+
+let numC = 10;
+let numD = 2;
+alert(numC / numD);
+
+let numE = 15;
+let numF = 9;
+alert(numE % numF);
+
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log(x);
+
+let y = 10;
+let z = 5;
+let x = y * z;
+console.log(x);
